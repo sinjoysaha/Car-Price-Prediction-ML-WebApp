712da44718963eddaaa4ef8a8d099fa38c55d0fb
sinjoysaha <sinjoysaha555@gmail.com>
2021-03-05 01:20:54 +0530

    Pkl Model load works, Building form in index.html

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request, redirect
+import pickle
+from sklearn.ensemble import RandomForestRegressor
 
 app = Flask(__name__)
 
+def model():
+    with open('model_rfr_rscv.pkl', 'rb') as f:
+        loaded_model = pickle.load(f)
+
+    return loaded_model
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    m = model()
+    m = str(m.best_params_)
+    return render_template('index.html', model=m)
 
 @app.route('/about')
 def about():
