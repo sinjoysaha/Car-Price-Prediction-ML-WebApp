2057a3dab7b9167faac1ac225b64695f8bb87eef
Sinjoy Saha <sinjoysaha555@gmail.com>
2023-10-08 16:11:54 +0530

    Removed about, contact pages

--- app.py
+++ app.py
@@ -69,14 +69,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+# @app.route('/about')
+# def about():
+#   return render_template('about.html')
 
 
-@app.route('/contact')
-def contact():
-  return render_template('contact.html')
+# @app.route('/contact')
+# def contact():
+#   return render_template('contact.html')
 
 
 if __name__ == "__main__":
